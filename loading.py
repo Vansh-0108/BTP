835371ec36c58c29ed8c25b728ce7573a0440e94
Vansh <vanshmahajan2001@gmail.com>
2023-09-03 22:23:33 +0530

    WoHoooo

---
+++ loading.py
@@ -0,0 +1,138 @@
+import os
+import torch
+import torchvision
+from torch.utils.data import random_split
+import torchvision.models as models
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision.datasets import ImageFolder
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+from torch.utils.data.dataloader import DataLoader
+from PIL import Image
+data_dir  = 'Garbage classification\Garbage classification'
+
+classes = os.listdir(data_dir)
+# print(classes)
+
+transformations = transforms.Compose([transforms.Resize((256, 256)), transforms.ToTensor()])
+
+dataset = ImageFolder(data_dir, transform = transformations)
+
+train_ds, val_ds, test_ds = random_split(dataset, [1804,173 ,550 ])
+# print(len(train_ds), len(val_ds), len(test_ds))
+
+
+def accuracy(outputs, labels):
+    _, preds = torch.max(outputs, dim=1)
+    return torch.tensor(torch.sum(preds == labels).item() / len(preds))
+
+class ImageClassificationBase(nn.Module):
+    def training_step(self, batch):
+        images, labels = batch
+        out = self(images)                  # Generate predictions
+        loss = F.cross_entropy(out, labels) # Calculate loss
+        return loss
+
+    def validation_step(self, batch):
+        images, labels = batch
+        out = self(images)                    # Generate predictions
+        loss = F.cross_entropy(out, labels)   # Calculate loss
+        acc = accuracy(out, labels)           # Calculate accuracy
+        return {'val_loss': loss.detach(), 'val_acc': acc}
+
+    def validation_epoch_end(self, outputs):
+        batch_losses = [x['val_loss'] for x in outputs]
+        epoch_loss = torch.stack(batch_losses).mean()   # Combine losses
+        batch_accs = [x['val_acc'] for x in outputs]
+        epoch_acc = torch.stack(batch_accs).mean()      # Combine accuracies
+        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
+
+    def epoch_end(self, epoch, result):
+        print("Epoch {}: train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(
+            epoch+1, result['train_loss'], result['val_loss'], result['val_acc']))
+        
+
+class ResNet(ImageClassificationBase):
+    def __init__(self):
+        super().__init__()
+        # Use a pretrained model
+        #using the resnet model available already
+        self.network = models.resnet50(pretrained=False)
+        # Replace last layer
+        num_ftrs = self.network.fc.in_features
+        self.network.fc = nn.Linear(num_ftrs, len(dataset.classes))
+
+    def forward(self, xb):
+        return torch.sigmoid(self.network(xb))
+
+model = ResNet()
+# batch_size=32
+# train_dl = DataLoader(train_ds, batch_size, shuffle = True, num_workers = 4, pin_memory = True)
+# val_dl = DataLoader(val_ds, batch_size*2, num_workers = 4, pin_memory = True)
+
+
+def get_default_device():
+    """Pick GPU if available, else CPU"""
+    if torch.cuda.is_available():
+        return torch.device('cuda')
+    else:
+        return torch.device('cpu')
+
+def to_device(data, device):
+    """Move tensor(s) to chosen device"""
+    if isinstance(data, (list,tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
+
+class DeviceDataLoader():
+    """Wrap a dataloader to move data to a device"""
+    def __init__(self, dl, device):
+        self.dl = dl
+        self.device = device
+
+    def __iter__(self):
+        """Yield a batch of data after moving it to device"""
+        for b in self.dl:
+            yield to_device(b, self.device)
+
+    def __len__(self):
+        """Number of batches"""
+        return len(self.dl)
+    
+def evaluate(model, val_loader):
+    model.eval()
+    outputs = [model.validation_step(batch) for batch in val_loader]
+    return model.validation_epoch_end(outputs)
+
+device = get_default_device()
+# print(device)
+
+# train_dl = DeviceDataLoader(train_dl, device)
+# val_dl = DeviceDataLoader(val_dl, device)
+model = ResNet()
+
+def predict_image(img, model):
+    # Convert to a batch of 1
+    xb = to_device(img.unsqueeze(0), device)
+    # Get predictions from model
+    yb = model(xb)
+    # Pick index with highest probability
+    prob, preds  = torch.max(yb, dim=1)
+    # Retrieve the class label
+    return dataset.classes[preds[0].item()]
+
+model = torch.load('BTP_model.pt',map_location=torch.device('cpu'))
+# print(model.eval())
+
+for i in range(100):
+    print()
+# while(True):
+    # i = int(input('Enter the testing ID:'))
+img = Image.open('g.jpeg')
+img = transformations(img)
+print('Predicted:', predict_image(img, model))
+
+
+
+
